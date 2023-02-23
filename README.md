e615d13df463328637f6cd8be5259abac3f37ca8
ddoris2106 <daundre.doris16@myhunter.cuny.edu>
2023-02-23 13:22:25 -0500

    Set up initial files with create react app

---
+++ README.md
@@ -0,0 +1,4 @@
+A clome of the Human Benchmark Chimp Test
+A mini-game to test your memory
+
+Link to Original: https://humanbenchmark.com/tests/chimp
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import GameApp from "./GaneApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<GameApp />);
